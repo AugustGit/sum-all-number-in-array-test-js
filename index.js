938fdd7d902d2044318aeb1206c7bc9d2c1340e4
austin metka <austin_metka@hotmail.com>
2018-02-16 18:54:17 +0000

    new recursive function added

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 function sumItems(array) {
+  let sum = 0;
+
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      sum += sumItems(item)
+    } else {
+    sum += item;
+      }
+})
+  return sum;
   // Sum all the numbers in the array
 }
 
